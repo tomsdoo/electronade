658748b942ae4f0cfcaa328a0840197601cd9993
tom <toms@live.jp>
2022-08-17 19:23:54 +0900

    main file is added

---
+++ src/main/index.ts
@@ -0,0 +1,19 @@
+import { ipcMain } from "electron";
+
+type Handle = {
+  eventName: string;
+  handler: Function;
+};
+type Handles = {
+  [key: string]: Handle[];
+};
+
+export function setHandles(handles: Handles){
+  Object.values(handles)
+    .forEach(arrayOfHandles => {
+      arrayOfHandles.forEach(({ eventName, handler }) => {
+        // @ts-ignore
+        ipcMain.handle(eventName, handler);
+      });
+    });
+}
